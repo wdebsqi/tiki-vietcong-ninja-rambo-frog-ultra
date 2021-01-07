02524f20271f9dfa148c4aa9e49589707360a4fb
bfokss <foks.bartosz@gmail.com>
2021-01-07 16:57:36 +0100

    Checking if git branch works

---
+++ Assets/Scenes/Character2DController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal") ;
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
